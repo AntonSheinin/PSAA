62bc351b878ef4ee9b08eca43c9037827e25540f
AntonSheinin <anton.sheinin@gmail.com>
2022-07-28 15:17:40 +0300

    fix

--- analyze_module/main.py
+++ analyze_module/main.py
@@ -6,7 +6,7 @@ from celery import Celery
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-app = Celery('main', broker='pyamqp://')
+app = Celery('main', backend='pyampq://', broker='pyamqp://guest@localhost//')
 
 def get_files_list(directory):
 
