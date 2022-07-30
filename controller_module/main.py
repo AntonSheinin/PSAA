6fc384494332e331911b829ebdad7f5db1fb8683
AntonSheinin <anton.sheinin@gmail.com>
2022-07-30 10:11:47 +0300

    fix

--- controller_module/main.py
+++ controller_module/main.py
@@ -7,9 +7,7 @@ import time
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-time.sleep(30)
-
-tasks = []
+#tasks = []
 
 app = Celery('main', broker='pyamqp://user:bitnami@rabbitmq', backend='rpc://user:bitnami@rabbitmq')
 
@@ -17,50 +15,34 @@ app.conf.update(task_serializer='json',
                 accept_content=['json'],  
                 result_serializer='json')
 
-time.sleep(20)
+#time.sleep(30)
 
-#print('1. Search password', '\n', '2. Analyze files')
-#choise = input('Enter choise : ')
+#tasks.append(app.send_task('password'))
+#print('password search task sent')
+#tasks.append(app.send_task('analyze'))
+#print('file analize task sent')
 
-#if choise == '1':
-#    tasks.append(app.send_task('password'))
+#for task in tasks:
+#    result = task.get()
+#    print('Received result:', result)
+
+def main():
+
+    tasks = []
     
-#elif choise == '2':
-#    tasks.append(app.send_task('analyze'))
+    app.start()
 
-#else:
-#    print('wrong choice')
+    time.sleep(30)
 
-tasks.append(app.send_task('password'))
-print('password search task sent')
-tasks.append(app.send_task('analyze'))
-print('file analize task sent')
+    tasks.append(app.send_task('password'))
+    print('password search task sent')
+    tasks.append(app.send_task('analyze'))
+    print('file analize task sent')
 
-for task in tasks:
-    result = task.get()
-    print('Received result:', result)
+    for task in tasks:
+        result = task.get()
+        print('Received result:', result)
 
-
-
-#def main():
-#    app.start()
-#
-#    print('1. Search password', '\n', '2. Analyze files')
-#    choise = input('Enter choise : ')
-#
-#    if choise == '1':
-#        tasks.append(app.send_task('password'))
-#    
-#    elif choise == '2':
-#        tasks.append(app.send_task('analyze'))
-#
-#    else:
-#        print('wrong choice')
-#
-#    for task in tasks:
-#        result = task.get()
-#        print('Received result:', result)
-#
-#if __name__ == '__main__':
-#    logger.info("Controller module is running and listening...")
-#    main()
+if __name__ == '__main__':
+    logger.info("Controller module is running and listening...")
+    main()
