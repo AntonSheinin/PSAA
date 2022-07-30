885a6e0be640c50b15b4b6f5d513ccfd1a7eec29
AntonSheinin <anton.sheinin@gmail.com>
2022-07-30 20:11:02 +0300

    fix

--- analyze_module/main.py
+++ analyze_module/main.py
@@ -1,4 +1,5 @@
 import logging
+from msilib import _directories
 import os
 from collections import Counter
 from celery import Celery
@@ -13,7 +14,7 @@ def get_files_list(directory):
 
     files_list = {'count' : 0, 'files' : []}
 
-    for root, files in os.walk(directory):
+    for root, directories, files in os.walk(directory):
         for file_name in files:
             file_path = os.path.join(root, file_name)
             file_size = os.path.getsize(file_path)
