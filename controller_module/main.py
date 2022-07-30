3064307a244bc41fd1e4865c90f8e5ff9ec796ad
AntonSheinin <anton.sheinin@gmail.com>
2022-07-30 17:15:20 +0300

    fix

--- controller_module/main.py
+++ controller_module/main.py
@@ -10,14 +10,12 @@ import json
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-time.sleep(15)
+time.sleep(10)
 
 app = Celery('main', broker='pyamqp://user:bitnami@rabbitmq', backend='rpc://user:bitnami@rabbitmq')
 
-app.conf.task_queues = (Queue('password', routing_key='password'),
-                        Queue('analyze', routing_key='analyze'))
-
-#app.conf.update(result_serializer='json')
+app.conf.task_queues = (Queue('password'),
+                        Queue('analyze'))
 
 def main():
 
@@ -32,10 +30,10 @@ def main():
     for task in tasks:
         result = task.get()
         task_name = result['task_name']
-        logger.info('result for task' + task_name + 'received')
+        logger.info('result for task' + task_name + ' received')
         with open('./theHarvester/' + task_name + '.json', 'w') as file:
             json.dump(result, file)
-            logger.info('result for task' + task_name + 'saved to file ' + task_name + '.json')
+            logger.info('result for task' + task_name + ' saved to file ' + task_name + '.json')
         
 if __name__ == '__main__':
     logger.info('Controller module is started...')
